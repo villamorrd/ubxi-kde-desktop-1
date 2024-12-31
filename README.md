aef98f96117058dc3d3a7f43b891a987ae37555a
villamorrd <villamorrd@students.nu-moa.edu.ph>
2024-12-31 17:31:49 +0800

    chore: initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# ubxi-kde-desktop
---
+++ plasma/look-and-feel/org.rhinolinux.unicorn.desktop/contents/layouts/org.kde.plasma.desktop-layout.js
@@ -0,0 +1,6 @@
+loadTemplate("org.rhinolinux.desktop.defaultPanel")
+
+var desktopsArray = desktopsForActivity(currentActivity());
+for( var j = 0; j < desktopsArray.length; j++) {
+    desktopsArray[j].wallpaperPlugin = 'org.kde.image';
+}
